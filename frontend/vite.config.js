ee478859a9973a477a94922ef658ffc07b41b56a
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-07-10 09:37:39 +0300

    feat: added plugin for linting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,30 @@
 import { defineConfig } from "vite";
+import { EsLinter, linterPlugin } from "vite-plugin-linter";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((configEnv) => ({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    linterPlugin({
+      include: ["./src/**/*.js", "./src/**.*jsx"],
+      linters: [
+        new EsLinter({
+          configEnv: configEnv,
+          serveOptions: { clearCacheOnStart: true },
+        }),
+      ],
+    }),
+  ],
   preview: {
-    port: 3006,
+    port: 3000,
     strictPort: true,
   },
   server: {
-    port: 3006,
+    port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3006",
+    origin: "http://0.0.0.0:3000",
   },
-});
+}));
